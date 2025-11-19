4c393c22259d721a244a48405827194bd832dbbf
iGipxy <gesangrah@gmail.com>
2025-11-19 21:37:48 +0700

    G 1.0

--- punyaG.java
+++ punyaG.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class punyaG {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] jumlahPelanggan = new int[5];
+        System.out.println("Masukkan jumlah pelanggan untuk 5 cabang:");
         
+        for (int i = 0; i < jumlahPelanggan.length; i++) {
+            System.out.print("Cabang " + (i + 1) + ": ");
+            jumlahPelanggan[i] = sc.nextInt();
+        }
 
-
+        System.out.println();
+        sc.close();
     }
 
 }
